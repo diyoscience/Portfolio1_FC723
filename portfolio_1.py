08a26531dc95eced23ff3f6a52f1f36d81d8b022
Diyorbek <diyorbekjon0078@gmail.com>
2026-03-22 23:12:27 +0000

    Update portfolio_1.py
    Changes:
    1.refactored to make it easier for other to read
    2. introduced new class that will check if the input is valid or not
    3.introduced new class that controls how class being executed in order and combine the use of Checking_number class and EulideanAlgorithm class.
    4. added input taker to take input from directly keyboard by user!

--- portfolio_1.py
+++ portfolio_1.py
@@ -1,31 +1,59 @@
 '''The Euclidean Algorithm'''
-
+class Cheking_number:
+    def __init__(self,number1,number2):
+        self.number1=number1
+        self.number2=number2
+        
+    def check_valid_number(self):
+        try:
+            self.number1=int(self.number1)
+            self.number2=int(self.number2)
+            return True
+        except ValueError:
+            print('it is not valid input')
+            return False
+    
 class EuclideanAlgorithm:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
+    def __init__(self,number1,number2):
+        self.number1=number1
+        self.number2=number2
         
     def devisor(self):
-        while self.b!=0:
-            c=self.a % self.b 
-            self.a=self.b
-            self.b=c
+        print(f"The Greatest Common Devisor of {self.number1} and {self.number2}:")
+        while self.number2!=0:
+            remainder=self.number1 % self.number2 
+            self.number1=self.number2
+            self.number2=remainder
             
     def check_for_0(self):
-        if self.a==0:
-            return self.b
-        else:
-            return self.a
+        if self.number1==0:
+            return self.number2
+        elif self.number2==0:
+            return self.number1
         
     def declare_common_devisor(self):
-        return f" The Greatest common devisor of {self.a} and {self.b} is {self.check_for_0()} "
+        return self.check_for_0()
+    
+class Runner:
+    def __init__(self,number1,number2):
+        self.number1=number1
+        self.number2=number2
         
-divid=EuclideanAlgorithm(267,890) 
-divid.devisor()   
-print(divid.declare_common_devisor())           
+    def run_function(self):
+        checker=Cheking_number(self.number1, self.number2)
+        if not checker.check_valid_number():
+            return "Not valid input"
+        gcd_calculate=EuclideanAlgorithm(checker.number1, checker.number2)
+        gcd_calculate.devisor()
+        print(f"{gcd_calculate.declare_common_devisor()}")
         
     
+print("To find the Greatest Common Devisor(GCD)!")
+num1_for_GCD=input("Please enter the first number: ")
+num2_for_GCD=input("Please enter the second number: ")
 
+runner=Runner(num1_for_GCD , num2_for_GCD)
+runner.run_function()
 
 
 
